b887e6879efcc325aeb32c5d0cfdfc975aad2327
Jonas Petrovsky <jond@post.cz>
2015-11-07 00:30:58 +0100

    added manhattan method

--- ukol_2/src/PuzzleHeuristics.py
+++ ukol_2/src/PuzzleHeuristics.py
@@ -13,11 +13,22 @@ class PuzzleHeuristics(object):
 
     def _calc_wrong_tiles_c(self, state):
         count = 0
-        for g_tile_name, g_tile_coord in self.goal_state.iteritems():
+        for g_tile_name, g_tile_coord in self.goal_state['tiles'].iteritems():
             #print g_tile_name, g_tile_coord, state[g_tile_name]
-            if state[g_tile_name] != g_tile_coord:
+            if state['tiles'][g_tile_name] != g_tile_coord:
                 count += 1
         return count
+
+    def _calc_manhattan_d(self, state):
+        h_sum = 0
+        for g_tile_name, (g_x, g_y) in self.goal_state['tiles'].iteritems():
+            #print g_tile_name, g_tile_coord, state[g_tile_name]
+            man_d = abs(g_x - state['tiles'][(g_tile_name)][0]) + abs(g_y - state['tiles'][g_tile_name][1])
+            #print("%s: %d | ") % (g_tile_name, man_d),
+            h_sum += man_d
+        return h_sum
+
+
 
 
 
--- ukol_2/src/PuzzleWorld.py
+++ ukol_2/src/PuzzleWorld.py
@@ -6,26 +6,19 @@ class PuzzleWorld(object):
     def __init__(self):
         # State representation: tile value => (x,y)
         self.goal_state = {
-            1: (1,1), 2: (1,2), 3: (1,3),
-            8: (2,1), 0: (2,2), 4: (2,3),
-            7: (3,1), 6: (3,2), 5: (3,3)
-        }
-        """
-        self.goal_state = {
             'tiles': {
                 1: (1,1), 2: (1,2), 3: (1,3),
                 8: (2,1), 0: (2,2), 4: (2,3),
                 7: (3,1), 6: (3,2), 5: (3,3)
             },
             'moved_tile': None,
-            'h_value': None
+            'h_value': 0
         }
-        """
         # Path from start state to goal state
         self.solution_path = []
         # Heuristics type
-        self.h_type = 1 # MIN number of tiles on wrong positions.
-        #self.h_type = 2 # MIN sum of manhattan distances from goal posisitions.
+        #self.h_type = 1 # MIN number of tiles on wrong positions.
+        self.h_type = 2 # MIN sum of manhattan distances from goal posisitions.
         self.heuristics = PuzzleHeuristics(self.goal_state)
 
 
@@ -36,8 +29,9 @@ class PuzzleWorld(object):
         # Add start state to the path.
         self.solution_path.append(start_state)
         current_state = self.solution_path[-1]
+        current_state['h_value'] = self.heuristics.calculate_value(self.h_type,current_state)
         # Search for a solution.
-        while current_state != self.goal_state:
+        while current_state['tiles'] != self.goal_state['tiles']:
             print "======CUR STATE======"
             self._show_state(current_state)
             # Find possible moves against zero tile.
@@ -52,15 +46,14 @@ class PuzzleWorld(object):
             # Choose state with the lowest heuristics value.
             best_state = self._find_best_state(valid_states)
             # Check if the best state has a lower or the same h value than the current state.
-            cur_h_value = self.heuristics.calculate_value(self.h_type, current_state)
             # If yes, another step in solution was found.
-            if best_state['h_value'] <= cur_h_value:
+            if best_state['h_value'] <= current_state['h_value']:
                 self.solution_path.append(best_state)
                 current_state = best_state
             # If no, the puzzle has no solution - end the program.
             else:
                 print('No solution found in %d moves.') % len(self.solution_path)
-                print(self.solution_path)
+                #print(self.solution_path)
                 exit()
         # Goal state was reached
         print('The goal state was reached in %d moves.') % len(self.solution_path)
@@ -74,17 +67,14 @@ class PuzzleWorld(object):
         Generates a random start state.
         """
         state_a = {
-            5: (1,1), 4: (1,2), 0: (1,3),
-            6: (2,1), 1: (2,2), 8: (2,3),
-            7: (3,1), 3: (3,2), 2: (3,3)
+            'tiles': {
+                5: (1,1), 4: (1,2), 0: (1,3),
+                6: (2,1), 1: (2,2), 8: (2,3),
+                7: (3,1), 3: (3,2), 2: (3,3)
+            },
+            'moved_tile': None,
+            'h_value': None
         }
-
-        state_b = {
-            5: (1,1), 0: (1,2), 1: (1,3),
-            6: (2,1), 3: (2,2), 8: (2,3),
-            7: (3,1), 4: (3,2), 2: (3,3)
-        }
-
 
         return state_a
 
@@ -97,7 +87,7 @@ class PuzzleWorld(object):
             list
         """
         # Get zero tile coordinates
-        tile_zero = current_state[0]
+        tile_zero = current_state['tiles'][0]
         # Count new coordinates for 4 movements.
         x_up = tile_zero[0] - 1
         x_down = tile_zero[0] + 1
@@ -127,24 +117,23 @@ class PuzzleWorld(object):
             list: new states
         """
         new_states = []
-        zero_tile = current_state[0]
+        zero_tile = current_state['tiles'][0]
         for moved_tile in possible_moves:
             new_zero = moved_tile
             new_move_tile = zero_tile
-            new_state = current_state.copy()
-            new_state[0] = new_zero
-            new_state[self._get_tile_name_by_coordinates(current_state, moved_tile)] = new_move_tile
+            new_state = {}
+            new_state['tiles'] = current_state['tiles'].copy()
+            new_state['tiles'][0] = new_zero
+            new_state['tiles'][self._get_tile_name_by_coordinates(current_state, moved_tile)] = new_move_tile
             new_state['moved_tile'] = moved_tile
             new_states.append(new_state)
         # result
         return new_states
 
-
     def _get_tile_name_by_coordinates(self, state, coordinates):
-        for t_name, xy in state.iteritems():
+        for t_name, xy in state['tiles'].iteritems():
             if xy == coordinates:
                 return t_name
-        pass
 
 
     def _show_state(self, state):
@@ -155,46 +144,20 @@ class PuzzleWorld(object):
         Select a state with the lowest h value.
         If there are more states with same value, choose the one which will place a non-zero title on the right place.
         """
-        min_h = float("inf")
-        min_states = []
-        # Get states with lowest h value.
-        for state in new_states:
-            if state['h_value'] < min_h:
-                min_h = state['h_value']
-                min_states.append(state)
-        # If there is only one state with the value, return the result.
-        if len(min_states) == 1:
-            return min_states[0]
-        # Otherwise search the states and if necessary, skip the zero tile.
-        for state in min_states:
-            if state['moved_tile'] == self.solution_path[-1][0]:
-                continue
-            else:
-                return state
-        #return min(new_states, key = lambda x: x['h_value'])
+        return min(new_states, key=lambda x: x['h_value'])
 
     def _get_valid_states(self, new_states):
-        # The state cannot be same as start state.
+        # The state cannot be the same as start state.
         valid_states = []
         for state in new_states:
             if state['tiles'] != self.goal_state['tiles']:
                 valid_states.append(state)
         # The state cannot be same as the state before parent state.
-
-
-
-
-"""
-
-"""
-
-
-
-
-
-
-
-
-
-
-
+        if len(self.solution_path) == 1:
+            return valid_states
+        valid_states_2 = []
+        for state in valid_states:
+            if state['tiles'] != self.solution_path[-2]['tiles']:
+                valid_states_2.append(state)
+        # result
+        return valid_states_2
